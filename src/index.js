a093f36b45e45e73062da828881c1c248cd4b164
joypciu <usmanjoycse@gmail.com>
2022-01-05 23:16:00 +0600

    Joy bangla

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import foodRoute from './pages/food.js';
 import resturantRoute from './pages/resturant.js';
 const app = express();
-
+const port = process.env || 3000;
 app.use(bodyParser.json({ limit: '100mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json());
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
     status: 404,
   });
 });
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('running at http://localhost:3000');
 });
