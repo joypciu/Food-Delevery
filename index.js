7c4b194aec2597476461a605d9d8d2ff8db36578
joypciu <usmanjoycse@gmail.com>
2022-01-06 15:24:40 +0600

    added Procfile

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import foodRoute from './pages/food.js';
-import resturantRoute from './pages/resturant.js';
+import foodRoute from './src/pages/food.js';
+import resturantRoute from './src/pages/resturant.js';
 const app = express();
-const port = process.env || 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json({ limit: '100mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json());
@@ -15,8 +15,10 @@ app.use('/resturant', resturantRoute);
 
 app.get('/', (req, res) => {
   res.json({
-    request: 'get',
-    status: 404,
+    message: 'Its working',
+    'api entry': 2,
+    'path 1': '/food',
+    'path 2': '/resturant',
   });
 });
 app.listen(port, () => {
